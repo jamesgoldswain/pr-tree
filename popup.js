f7522466454d882d2c155343996a7cd23135c22e
james goldswain <james@ouimedia.com>
2018-04-04 10:02:36 +0800

    Update popup.js
    Cleanup and on some MRs for gitlab the specificity was wrong when generating the file tree

--- popup.js
+++ popup.js
@@ -7,127 +7,144 @@ $(function() {
     var observer = new MutationObserver(function(mutations) {
         console.log(mutations);
      });
+	
     // configuration of the observer:
     var config = { attributes: true, childList: true, characterData: true };
-
+    var pageCount = 0;
+    var site = $("meta[property='og:site_name']").attr("content").toLowerCase();
     var files = [];
     var fileTree = [];
     var fileTreeStructure = [];
 
+		var getKids = function(data) {
+				var arr = [];
+
+				Object.getOwnPropertyNames(data).forEach(
+						function (val, idx, array) {
+						if (data[val].children != null)
+								arr.push({ name : val, shortPath : data[val].shortPath, children : getKids(data[val].children) })
+						}
+				);
+				return arr;
+		};
+
+		var addnode = function(obj) {
+
+				var splitpath = obj.path.replace(/^\/|\/$/g, "").split('/');
+				var ptr = tree;
+
+				for (i=0;i<splitpath.length;i++)
+				{
+						node = { name: splitpath[i], type: 'directory', shortPath : obj.shortPath};
+
+						if(i == splitpath.length-1)
+							{node.shortPath = obj.shortPath; node.type = obj.type;}
+
+						ptr[splitpath[i]] = ptr[splitpath[i]] || node;
+						ptr[splitpath[i]].children=ptr[splitpath[i]].children||{};
+
+						ptr=ptr[splitpath[i]].children;
+				}    
+		};
+	
+    var buildGitLabData = function(data){
+        $('.diffs .file-header-content a').each(function(i,e){
+            var fileNameInfo = $(e).text();
+            var shortPath = $(e).attr('href').split('#')[1];
+            $(e).parents('.diff-file:first').attr('id', shortPath);
+            data.push( { shortPath: shortPath , path: 'r/' + fileNameInfo });
+        })
+    };
+
+    var buildGitHubData = function(data){
+        $('.file-info a').each(function(i,e){
+            var fileNameInfo = $(e).attr('title');
+            var shortPath = $(e).parents('.file:first').attr('id');
+            $(e).parents('.file:first').attr('id', shortPath);
+            $(e).text(fileNameInfo);
+            data.push( { shortPath: shortPath , path: 'r/' + fileNameInfo });
+        })
+    };
+		
     var loadUp = function(site){
 
         // only load in files diff mode
-        if (window.location.href.indexOf('diffs') > 0 || window.location.href.indexOf('files') > 0){
+        if (window.location.href.indexOf('diffs') > 0 || window.location.href.indexOf('files') > 0) {
 
-        var data = [];
+					var data = [];
 
-        switch(site){
-            case "gitlab" : buildGitLabData(data)
-                break;
-            default: buildGitHubData(data)
-        }
+					switch(site){
+							case "gitlab" : buildGitLabData(data)
+									break;
+							default: buildGitHubData(data)
+					}
 
-        var tree = {};
+					var tree = {};
+					var final = [];
 
-        function addnode(obj){
-            var splitpath = obj.path.replace(/^\/|\/$/g, "").split('/');
-            
-            var ptr = tree;
-            for (i=0;i<splitpath.length;i++)
-            {
-                node = { name: splitpath[i], type: 'directory', shortPath : obj.shortPath};
-                
-                if(i == splitpath.length-1)
-                	{node.shortPath = obj.shortPath; node.type = obj.type;}
+					data.map(addnode);
 
-                ptr[splitpath[i]] = ptr[splitpath[i]] || node;
-                ptr[splitpath[i]].children=ptr[splitpath[i]].children||{};
-                
-                ptr=ptr[splitpath[i]].children;
-            }    
-        }
+					final.push({name : 'root', children : getKids(tree.r.children) });
 
-		var final = [];
-        
-        data.map(addnode);
-        
-        final.push({name : 'root', children : getKids(tree.r.children) })
+					var t
 
-        if (site === 'gitlab'){
-            $('.files').before('<div id="tree" class="navigation"></div>');
-            $('#tree').before('<div id="hiddentree" class="navigation"><a href="">&raquo;</a></div>');
+					if (site === 'gitlab'){
+							$('.files').before('<div id="tree" class="navigation"></div>');
+							$('#tree').before('<div id="hiddentree" class="navigation"><a href="">&raquo;</a></div>');
 
-            var t = new TreeView(final, 'tree');
+							t = new TreeView(final, 'tree');
 
-            $('#hiddentree').on('click', function(e){
-                $('.diff-file').hide();
-                $('#hiddentree').hide();
-                $('#tree').show();
-                e.preventDefault();
-            });
+							$('#hiddentree').on('click', function(e){
+									$('.diff-file').hide();
+									$('#hiddentree').hide();
+									$('#tree').show();
+									e.preventDefault();
+							});
 
-            t.on('select', function (e) { 
-                $('.diff-file').hide();
-                $('#tree').hide();
-                $('#hiddentree').show();
-                var item = e.data.shortPath;
-                document.getElementById(e.data.shortPath).style.display = 'block';
-            })
+							t.on('select', function (e) { 
+									$('.diff-file').hide();
+									$('#tree').hide();
+									$('#hiddentree').show();
+									var item = e.data.shortPath;
+									document.getElementById(e.data.shortPath).style.display = 'block';
+							})
 
-            $('.diff-file').hide();
+							$('.diff-file').hide();
 
-        }else{
-            $('#files').before('<div id="tree" class="navigation"></div>');
-            $('#tree').before('<div id="hiddentree" class="navigation"><a href="">&raquo;</a></div>');
-    
-            var t = new TreeView(final, 'tree');
-    
-            $('#hiddentree').on('click', function(e){
-                $('.file').hide();
-                $('#hiddentree').hide();
-                $('#tree').show();
-                e.preventDefault();
-            });
-    
-            t.on('select', function (e) { 
-                $('.file').hide();
-                $('#tree').hide();
-                $('#hiddentree').show();
-                var item = e.data.shortPath;
-                document.getElementById(e.data.shortPath).style.display = 'block';
-            });
-        }
+					} else {
+							$('#files').before('<div id="tree" class="navigation"></div>');
+							$('#tree').before('<div id="hiddentree" class="navigation"><a href="">&raquo;</a></div>');
 
-       function getKids(data){
-       	
-                var arr = [];
-                    
-                Object.getOwnPropertyNames(data).forEach(
-                    function (val, idx, array) {
-                    if (data[val].children != null)
-                        arr.push({ name : val, shortPath : data[val].shortPath, children : getKids(data[val].children) })
-                    }
-                );
-                return arr;
-            }
+							t = new TreeView(final, 'tree');
 
-        }
-    }
+							$('#hiddentree').on('click', function(e){
+									$('.file').hide();
+									$('#hiddentree').hide();
+									$('#tree').show();
+									e.preventDefault();
+							});
 
-    var pageCount = 0;
-    var site = $("meta[property='og:site_name']").attr("content").toLowerCase();
+							t.on('select', function (e) { 
+									$('.file').hide();
+									$('#tree').hide();
+									$('#hiddentree').show();
+									var item = e.data.shortPath;
+									document.getElementById(e.data.shortPath).style.display = 'block';
+							});
+					}
+				}
+    };
 
-    var pager = window.setInterval(function(){
+    var pager = window.setInterval( function(){
 
         if (site === 'gitlab'){
             if ($('.loading:visible').length === 0){
 
                 window.clearInterval(pager);
-    
                 loadUp(site);
-            
             }
         }
+			
         if (site === 'github'){
             $('html, body').animate({
                 scrollTop: $(".footer").offset().top
@@ -145,25 +162,5 @@ $(function() {
             
             }
         }
-        
-    }, 1000)
-
-    var buildGitLabData = function(data){
-        $('.file-header-content a').each(function(i,e){
-            var fileNameInfo = $(e).text();
-            var shortPath = $(e).attr('href').split('#')[1];
-            $(e).parents('.diff-file:first').attr('id', shortPath);
-            data.push( { shortPath: shortPath , path: 'r/' + fileNameInfo });
-        })
-    }
-
-    var buildGitHubData = function(data){
-        $('.file-info a').each(function(i,e){
-            var fileNameInfo = $(e).attr('title');
-            var shortPath = $(e).parents('.file:first').attr('id');
-            $(e).parents('.file:first').attr('id', shortPath);
-            $(e).text(fileNameInfo);
-            data.push( { shortPath: shortPath , path: 'r/' + fileNameInfo });
-        })
-    }
+    }, 1000);
 });
